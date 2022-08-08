0af9a820d2dc26c640ee6989850c82eff740e49b
João Vanazzi <joao.vanazzi@guiainvest.com.br>
2022-08-07 23:29:07 -0300

    Created the Aggregator class to implement aggregation functions and syntax.

---
+++ Lib.SqlQueryBuilder/Operators/Aggregator.cs
@@ -0,0 +1,15 @@
+﻿namespace Lib.QueryBuilder.Operators;
+
+public static class Aggregator
+{
+    public static string Avg() => "AVG(*)";
+    public static string Avg(string column) => $"AVG({column})";
+    public static string Count() => "COUNT(*)";
+    public static string Count(string column) => $"COUNT({column})";
+    public static string Max() => "MAX(*)";
+    public static string Max(string column) => $"MAX({column})";
+    public static string Min() => "MIN(*)";
+    public static string Min(string column) => $"MIN({column})";
+    public static string Sum() => "SUM(*)";
+    public static string Sum(string column) => $"SUM({column})";
+}
